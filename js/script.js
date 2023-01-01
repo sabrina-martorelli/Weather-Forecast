55055d64fc1f1739a43cd7345d7d52fdcf3ba30b
SLM <sabrina.martorelli@gmail.com>
2023-01-01 12:06:41 -0300

    Adds console log for debugging

--- js/script.js
+++ js/script.js
@@ -8,13 +8,14 @@ var key ='be11bc1fc11463fbd25d54db69d7b0ba';
 function getForecast(event) {
 
     var searchCity = weatherSearch.val().trim();
+    console.log('outside function');
 
     if (searchCity) {
         
       $.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${key}&units=metric`)
       .then(function (data) {
           //displayMatches(data.Search);
-          console.log('inside fucntion');
+          console.log('inside function');
           console.log(data);
         });
   
@@ -29,6 +30,7 @@ function getForecast(event) {
 function init() {
  
     searchButton.on('click', getForecast);
+    console.log('after function call');
 
   }
   
