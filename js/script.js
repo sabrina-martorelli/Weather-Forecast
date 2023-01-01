9957c9012331143808bd38b32a2fee5edd67f84b
SLM <sabrina.martorelli@gmail.com>
2023-01-01 11:52:55 -0300

    Adds url and key to get api data

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 
 var searchButton = document.querySelector('.search-button');
 
-var weatherSearch = document.querySelector('.weather-search')
+var weatherSearch = document.querySelector('.weather-search');
+
+var key ='be11bc1fc11463fbd25d54db69d7b0ba';
 
 
+//Function to get Forecast information of searched city
 function getForecast(event) {
 
     var searchCity = weatherSearch.val().trim();
 
     if (searchCity) {
-        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}&units=metric`
-
-      $.get(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`)
+        
+      $.get(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=${key}&units=metric`)
       .then(function (data) {
-          displayMatches(data.Search);
+          //displayMatches(data.Search);
+          console.log(data);
         });
   
   
@@ -23,7 +26,7 @@ function getForecast(event) {
 
 
 
-//Event listener for  search button click
+//Event listener for search button
 function init() {
  
     searchButton.on('click', getForecast);
