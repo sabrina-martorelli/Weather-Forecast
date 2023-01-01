960590f8958e44d2d235952eae8896b444c88728
SLM <sabrina.martorelli@gmail.com>
2023-01-01 11:46:13 -0300

    Adds code for getForcast funtion

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 
-var searchButton = document.querySelector('.btn search-button');
+var searchButton = document.querySelector('.search-button');
+
+var weatherSearch = document.querySelector('.weather-search')
+
+
+function getForecast(event) {
+
+    var searchCity = weatherSearch.val().trim();
+
+    if (searchCity) {
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}&units=metric`
+
+      $.get(`https://www.omdbapi.com/?apikey=20dc4c7f&s=${searchText}`)
+      .then(function (data) {
+          displayMatches(data.Search);
+        });
+  
+  
+  
+    }
+  }
 
 
 
+//Event listener for  search button click
 function init() {
-    searchButton.keydown(getForecast);
+ 
+    searchButton.on('click', getForecast);
+
   }
   
   init();
