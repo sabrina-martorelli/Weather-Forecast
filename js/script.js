9adf0e5952311ac70ec60b3ddc5290a0bec70880
SLM <sabrina.martorelli@gmail.com>
2023-01-01 13:17:46 -0300

    Adds preventDefault and cleans code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,60 @@
-
+var key ='be11bc1fc11463fbd25d54db69d7b0ba';
 var searchButton = $('.search-button');
 var weatherSearch = $('.weather-search');
-var key ='be11bc1fc11463fbd25d54db69d7b0ba';
-var searchCity ='London';
+var showToday = $('#today');
 
 
-    // $.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${key}&units=metric`)
-    //   .then(function (data) {
-        
-    //       console.log('test ');
-    //       console.log(data);
-    //   });
+
+
+function displayForecast(result){
+    console.log(result);
+        if (!result) {
+            showToday.html('<p >No result found </p>');
+           
+            return;
+         }
+    
+    }
+
+
+
 
 //Function to get Forecast information of searched city
 function getForecast(event) {
 
-    // var searchCity = weatherSearch.val().trim();
-    // console.log(searchCity);
+    event.preventDefault();
+
+    var searchCity = weatherSearch.val().trim();
+    console.log(searchCity);
 
     if (searchCity) {
         
       $.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${key}&units=metric`)
       .then(function (data) {
-          displayMatches(data.Search);
-          console.log('inside function');
-          console.log(data);
-        });
+        
+       displayForecast(data);
+         
+       });
+       
   
-  
-  
-     }
   }
+
+  }
+
+
 
 
 
 //Event listener for search button
-function init() {
- 
-    searchButton.on('click', getForecast);
-    console.log('after function call');
 
-  }
+function init() {
+    searchButton.click(getForecast);
+   
+  };
   
-init();
+  init();
+
+
+    
+  
+  
